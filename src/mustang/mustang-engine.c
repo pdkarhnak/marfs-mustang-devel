663187021ad9f28740033c0d4db724b4c857820e
Paul D. Karhnak <pkarhnak@lanl.gov>
2024-06-19 13:29:32 -0600

    Renamed testmain.c; added some argument-validation and error handling code.

--- src/mustang/mustang-engine.c
+++ src/mustang/mustang-engine.c
@@ -1,57 +1,119 @@
-#include <stdlib.h>
 #include <stdio.h>
-#include <pthread.h>
-#include "hashtable/hashtable.h"
+#include <string.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+
+#ifdef DEBUG
+#include <assert.h>
+#define SHORT_ID() (pthread_self() & 0xFFFF)
+#endif
+
+#include "hashtable.h"
+#include "mustang_threading.h"
 #include "pthread_vector.h"
+
+#define WARN(message, errorcode) \
+    printf("WARN: %s (%s)\n", message, strerror(errorcode));
+
+#define ERR(message, errorcode) \
+    printf("ERROR: %s (%s)\n", message, strerror(errorcode));
+
+extern void* thread_routine(void* args);
 
 int main(int argc, char** argv) {
 
-    size_t max_threads = argv[1];
+    int retval = 0;
 
-    int arg_index = 3;
-
-    int pathcount = 0;
-    int nscount = 0;
-    // "[namespaces:]"
-
-    // pretend like you\'re doing an unconditional traversal from root every time--it\'ll simplify things
-
-    /*
-    for ( ; arg_index < argc; arg_index += 1) {
-        if (strncmp(argv[arg_index], "[namespaces:]", strlen(argv[arg_index])) == 0) {
-            arg_index += 1;
-            break;
-        }
-
-        pathcount += 1;
+    if (argc < 4) {
+        printf("USAGE: ./mustang-engine [output file] [max threads] [paths, ...]\n");
+        printf("\tHINT: see mustang wrapper or invoke \"mustang -h\" for more details.\n");
+        return 1;
     }
 
-    for ( ; arg_index < argc; arg_index += 1) {
-        nscount += 1;
+    char* output_filepath = argv[1];
+    FILE* output_ptr = fopen(output_filepath, "w");
+
+    if (output_ptr == NULL) {
+        printf("ERROR: failed to open file \"%s\"\n", argv[1]);
+        return 1;
     }
-    */
 
-    // Prepare hashtable and associated mutex
-    hashtable* object_names_hashtable = init_table();
-    pthread_mutex_t* hashtable_lock_ref = (pthread_mutex_t*) calloc(1, sizeof(pthread_mutex_t));
-    pthread_mutex_init(hashtable_lock_ref, NULL);
+    hashtable* output_table = hashtable_init();
+    pthread_mutex_t* ht_lock = (pthread_mutex_t*) calloc(1, sizeof(pthread_mutex_t));
+    pthread_mutex_init(ht_lock, NULL);
 
-    // Prepare sync primitives to sync on active number of threads
-    pthread_mutex_t* threads_active_lock_ref = (pthread_mutex_t*) calloc(1, sizeof(pthread_mutex_t));
-    pthread_mutex_init(threads_active_lock_ref, NULL);
+    const size_t max_threads = ((size_t) atol(argv[2]));
 
-    pthread_cond_t* threads_active_cv_ref = (pthread_cond_t*) calloc(1, sizeof(pthread_cond_t));
-    pthread_cond_init(threads_active_cv_ref, NULL);
+    threadcount_verifier* verifier = verifier_init(max_threads);
 
-    // Prepare data and sync primitives for the pthread_t array to allow safe join, expansion, etc.
-    pthread_t* thread_ids = (pthread_t*) calloc(max_threads, sizeof(pthread_t));
-    pthread_mutex_t* thread_ids_lock_ref = (pthread_mutex_t*) calloc(1, sizeof(pthread_mutex_t));
-    pthread_mutex_init(thread_ids_lock_ref, NULL);    
+#ifdef DEBUG
+    pthread_mutex_t* out_lock = (pthread_mutex_t*) calloc(1, sizeof(pthread_mutex_t));
+    pthread_mutex_init(out_lock, NULL);
+#endif
 
-    pthread_vector* pthread_ts_vector = pthread_vector_init(max_threads);
+    pthread_vector* pt_vec = pthread_vector_init(max_threads);
 
+    if ((output_table == NULL) || (errno == ENOMEM)) {
+        ERR("No memory left--failed to allocate hashtable.", errno)
+        return 1;
+    }
 
-    // TODO: write hashtable contents to a file node-by-node before destroying
-    destroy_table(object_names_hashtable);
+    thread_args* top_args = (thread_args*) calloc(1, sizeof(thread_args));
+
+    if ((top_args == NULL) || (errno == ENOMEM)) {
+        ERR("No memory left--failed to allocate thread_args struct.", errno);
+        return 1;
+    }
+
+    top_args->tc_verifier = verifier;
+    top_args->pt_vector = pt_vec;
+    top_args->hashtable = output_table;
+    top_args->hashtable_lock = ht_lock;
+    top_args->basepath = strdup(argv[2]);
+    top_args->cwd_fd = open(argv[2], O_RDONLY | O_DIRECTORY);
+
+#ifdef DEBUG
+    top_args->stdout_lock = out_lock;
+#endif
+
+    pthread_t top_thread;
+    pthread_create(&top_thread, NULL, &thread_routine, (void*) top_args);
+
+#ifdef DEBUG
+    pthread_mutex_lock(out_lock);
+    printf("[thread %0lx -- parent]: created top thread with ID: %0lx\n", SHORT_ID(), (top_thread & 0xFFFF));
+    pthread_mutex_unlock(out_lock);
+#endif
+
+    pthread_vector_appendset(pt_vec, &top_thread, 1);
+    
+    size_t* perthread_retval;
+
+    for (int index = 0; index < (pt_vec->size); index += 1) {
+        pthread_vector_pollthread(pt_vec, (void**) &perthread_retval, index);
+#ifdef DEBUG
+        pthread_mutex_lock(out_lock);
+        printf("[thread %0lx -- parent]: Thread at index %d returned: %zu\n", SHORT_ID(), index, ((size_t) perthread_retval));
+        pthread_mutex_unlock(out_lock);
+#endif
+    }
+
+#ifdef DEBUG
+    pthread_mutex_destroy(out_lock);
+    free(out_lock);
+    out_lock = NULL;
+#endif
+
+    hashtable_dump(output_table, output_ptr);
+    fclose(output_ptr);
+
+    verifier_destroy(verifier);
+    pthread_vector_destroy(pt_vec);
+    hashtable_destroy(output_table);
+    pthread_mutex_destroy(ht_lock);
+    free(ht_lock);
+
+    return 0;
 
 }
