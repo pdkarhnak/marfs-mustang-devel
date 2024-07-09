cd1b0a7d763b37aaa4872857dccb9248916c3f83
Paul D. Karhnak <pkarhnak@lanl.gov>
2024-07-09 15:00:57 -0600

    First-draft implementation of the ID cache interface; moving to test.

---
+++ src/mustang/id_cache.c
@@ -0,0 +1,137 @@
+#include "id_cache.h"
+#include <errno.h>
+
+/* Private functions */
+id_cachenode* cachenode_init(char* new_id) {
+    id_cachenode* new_cachenode = (id_cachenode*) calloc(1, sizeof(new_cachenode));
+
+    if (new_cachenode == NULL) {
+        return NULL;
+    }
+
+    new_cachenode->prev = NULL;
+    new_cachenode->next = NULL;
+
+    new_cachenode->id = strdup(new_id);
+
+    if (new_cachenode->id == NULL) {
+        free(new_cachenode);
+        return NULL;
+    }
+
+    return new_cachenode;
+}
+
+void cachenode_destroy(id_cachenode* to_destroy) {
+    if (to_destroy == NULL) {
+        return;
+    }
+
+    free(to_destroy->id);
+
+    if (to_destroy->prev != NULL) {
+        to_destroy->prev->next = to_destroy->next;
+    }
+
+    if (to_destroy->next != NULL) {
+        to_destroy->next->prev = to_destroy->prev;
+    }
+
+    to_destroy->next = NULL;
+    to_destroy->prev = NULL;
+    free(to_destroy);
+}
+
+id_cache* id_cache_init(size_t new_capacity) {
+    id_cache* new_cache = calloc(1, sizeof(id_cache));
+
+    if (new_capacity == NULL) {
+        return NULL;
+    }
+
+    new_cache->head = NULL;
+    new_cache->tail = NULL;
+    new_cache->size = 0;
+    new_cache->capacity = new_capacity;
+
+    return new_cache;
+}
+
+int id_cache_probe(id_cache* cache, char* searched_id) {
+    if ((cache == NULL) || (searched_id == NULL)) {
+        return 0;
+    }
+
+    if (cache->size == 0) {
+        return 0;
+    }
+
+    id_cachenode* searched_node = cache->head;
+
+    do {
+        if (strncmp(searched_node->id, searched_id, strlen(searched_node->id)) == 0) {
+            if (searched_node != cache->head) {
+                searched_node->next->prev = searched_node->prev;
+                searched_node->prev->next = searched_node->next;
+
+                searched_node->prev = NULL;
+                searched_node->next = cache->head;
+
+                cache->head = searched_node;
+            }
+
+            return 1;
+        }
+
+        searched_node = searched_node->next;
+    } while (searched_node != NULL);
+
+    return 0;
+}
+
+int id_cache_add(id_cache* cache, char* new_id) {
+    id_cachenode* added_node = cachenode_init(new_id);
+
+    if (added_node == NULL) {
+        return -1;
+    }
+
+    added_node->next = cache->head;
+    cache->head = added_node;
+
+    // If the cache was empty at the time of this addition, also mark the new 
+    // node as the cache's tail node.
+    if (cache->size == 0) {
+        cache->tail = added_node;
+    }
+
+    // Unconditionally increment size, then clean up to enforce capacity as 
+    // needed.
+    cache->size += 1; 
+
+    if (cache->size > cache->capacity) {
+        cachenode_destroy(cache->tail);
+        cache->size -= 1;
+    }
+
+    return 0;
+}
+
+void id_cache_destroy(id_cache* cache) {
+    if (cache == NULL) {
+        return;
+    }
+
+    id_cachenode* destroyed_node = cache->head;
+
+    do {
+        id_cachenode* next_ref = destroyed_node->next;
+        cachenode_destroy(destroyed_node);
+        destroyed_node = next_ref;
+    } while (destroyed_node != NULL);
+
+    cache->head = NULL;
+    cache->tail = NULL;
+    cache->size = 0;
+    free(cache);
+}
---
+++ src/mustang/id_cache.h
@@ -0,0 +1,58 @@
+#ifndef __ID_CACHE_H__
+#define __ID_CACHE_H__
+
+#include <stdlib.h>
+
+typedef struct id_cachenode_struct id_cachenode;
+
+typedef struct id_cachenode_struct {
+    char* id;
+    id_cachenode* prev;
+    id_cachenode* next;
+} id_cachenode;
+
+typedef struct id_cache_struct id_cache;
+
+typedef struct id_cache_struct {
+    size_t size;
+    size_t capacity;
+    id_cachenode* head;
+    id_cachenode* tail;
+} id_cache;
+
+/**
+ * Allocate space for, and return a pointer to, a new id_cache struct on the 
+ * heap according to a specified capacity.
+ *
+ * Returns: valid pointer to id_cache struct on success, or NULL on failure.
+ */
+id_cache* id_cache_init(size_t new_capacity);
+
+/** 
+ * Check an id_cache struct for a node which stores ID `searched_id`. If the ID
+ * is stored, silently "bump" the node storing the searched ID to the head of 
+ * the cache to indicate that the ID has been the most recently used in the
+ * cache.
+ *
+ * Returns: 1 if a node was found which stored an id matching `stored_id`, or 0
+ * if no such node was present.
+ */
+int id_cache_probe(id_cache* cache, char* searched_id);
+
+/** 
+ * Create a new cache node to store `new_id`, then place that node at the head
+ * of the cache to indicate that the `new_id` is the most-recently-used ID in
+ * the cache. If the cache is at capacity, silently evict the tail node in the
+ * cache to make room for the new node.
+ *
+ * Returns: 0 on success (node could be created and the list was successfully
+ * modified), or -1 on failure (node could not be allocated).
+ */
+int id_cache_add(id_cache* cache, char* new_id);
+
+/**
+ * Destroy the given id_cache struct and free the memory associated with it.
+ */
+void id_cache_destroy(id_cache* cache);
+
+#endif
