ee9fd276cf52c8131b7fbce2bb1647ce9675fd9e
Paul D. Karhnak <pkarhnak@lanl.gov>
2024-07-10 07:22:32 -0600

    Black-box commit for id_cache_init

--- src/mustang/id_cache.c
+++ src/mustang/id_cache.c
@@ -3,7 +3,14 @@
 #include <errno.h>
 
 id_cache* id_cache_init(size_t new_capacity) {
-    return NULL;
-    // id_cache* new_cache = (id_cache*) calloc(1, sizeof(id_cache));
+    id_cache* new_cache = (id_cache*) calloc(1, sizeof(id_cache));
+
+    if (new_cache == NULL) {
+        return NULL;
+    }
+
+    new_cache->capacity = new_capacity;
+
+    return new_cache;
 }
 
