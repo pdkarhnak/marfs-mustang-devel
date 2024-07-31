42ec46dd6bbe0af16325760c1a9869d3643ac6da
Paul D. Karhnak <pkarhnak@lanl.gov>
2024-07-31 12:33:22 -0600

    Furnished documentation for mustang_threading interface; provided inline comments for get_ftag() helper.

--- src/mustang/mustang_threading.c
+++ src/mustang/mustang_threading.c
@@ -83,8 +83,37 @@ GNU licenses can be found at http://www.gnu.org/licenses/.
 #define LOG_PREFIX "mustang_threading"
 #include <logging/logging.h>
 
+/**
+ * Open the file at `path` using the context of the current MarFS position
+ * `current_position` and the associated MDAL `current_mdal`. Then, query the 
+ * string representation of the file's FTAG by getting the xattr with key 
+ * "FTAG_NAME" and return the result (if valid and matching the expected 
+ * length from an initial check) as a heap-allocated string. Close the file 
+ * before returning.
+ *
+ * Returns: pointer to a valid FTAG string representation on success, or NULL 
+ * on failure.
+ *
+ * NOTE: This function may return NULL and potentially set errno in any of the
+ * following cases:
+ * - The file open call using current_mdal->open() fails.
+ * - The first extended attribute query using current_mdal->fgetxattr() fails
+ *   (errno set to ENOATTR, which is reported as ENODATA if the system does not
+ *   define ENOATTR).
+ * - The calloc() call to get heap space for the FTAG string representation 
+ *   fails (errno set to ENOMEM).
+ * - The length of the string value returned in the second 
+ *   current_mdal->fgetxattr() call does not match the expected length as 
+ *   returned from the first current_mdal->fgetxattr() call (errno set to 
+ *   ESTALE).
+ */
 char* get_ftag(marfs_position* current_position, MDAL current_mdal, char* path) {
+    // gets MDAL file handle as a file descriptor equivalent.
+    // MDAL_FHANDLE the first argument to MDAL->fgetxattr() like how a file
+    // descriptor is the first argument to the standard POSIX fgetxattr().
     MDAL_FHANDLE target_handle = current_mdal->open(current_position->ctxt, path, O_RDONLY);
+
+    // Can't do anything with a bad handle--exit immediately.
     if (target_handle == NULL) {
         return NULL;
     }
@@ -93,6 +122,7 @@ char* get_ftag(marfs_position* current_position, MDAL current_mdal, char* path) 
     // 1 == hidden xattr
     ssize_t ftag_len = current_mdal->fgetxattr(target_handle, 1, FTAG_NAME, ftag_str, 0); // Fetch initial length of ftag
 
+    // Length < 0 indicates error, and 0 length means there is no data to get.
     if (ftag_len <= 0) {
         current_mdal->close(target_handle);
         errno = ENOATTR;
@@ -107,6 +137,10 @@ char* get_ftag(marfs_position* current_position, MDAL current_mdal, char* path) 
         return NULL; 
     }
 
+    // xattr value changed between first call (query for length) and this call
+    // (query for value itself). May be considered a "data race", but this code
+    // catches unexpected differences in a way that will get reported as an
+    // error by the caller.
     if (current_mdal->fgetxattr(target_handle, 1, FTAG_NAME, ftag_str, ftag_len) != ftag_len) {
         current_mdal->close(target_handle);
         free(ftag_str);
@@ -114,10 +148,23 @@ char* get_ftag(marfs_position* current_position, MDAL current_mdal, char* path) 
         return NULL;
     }
 
+    // Clean up after the open---caller does not need file handle kept open.
     current_mdal->close(target_handle);
     return ftag_str;
 }
 
+/**
+ * Using a task's parameters, traverse the directory that the current setting 
+ * of `task_position` corresponds to, reading all directory entries and acting
+ * accordingly. If an entry corresponds to a regular file, get its FTAG and its
+ * object ID(s), then store the ID(s). If an entry corresponds to a directory, 
+ * create a new task for that directory bundled with appropriate state for a 
+ * worker thread to complete.
+ *
+ * NOTE: This function always returns, including on failure. Failures are 
+ * always logged to the logfile passed as a program argument since all 
+ * build settings at least log errors.
+ */
 void traverse_dir(marfs_config* base_config, marfs_position* task_position, hashtable* output_table, pthread_mutex_t* table_lock, task_queue* pool_queue) {
     id_cache* this_id_cache = id_cache_init(id_cache_capacity);
 
@@ -316,6 +363,18 @@ void traverse_dir(marfs_config* base_config, marfs_position* task_position, hash
     free(task_position);
 }
 
+/**
+ * Using a task's parameters, check the namespace that the current setting of
+ * `task_position` corresponds to for subspaces. Create new tasks, bundled with
+ * appropriate state, for any subspaces (i.e., nested namespaces) for worker 
+ * threads to complete. Then, treat this namespace as a directory and check for
+ * "standard" (i.e., regular file and directory) contents by calling 
+ * traverse_dir().
+ *
+ * NOTE: Like traverse_dir(), this function always returns, including on 
+ * failure. Failures are always logged to the relevant logfile since all build
+ * settings for MUSTANG log errors.
+ */
 void traverse_ns(marfs_config* base_config, marfs_position* task_position, hashtable* output_table, pthread_mutex_t* table_lock, task_queue* pool_queue) {
     // Attempt to fortify the thread's position (if not already fortified) and check for errors
     if ((task_position->ctxt == NULL) && config_fortifyposition(task_position)) {
@@ -362,6 +421,12 @@ void traverse_ns(marfs_config* base_config, marfs_position* task_position, hasht
     traverse_dir(base_config, task_position, output_table, table_lock, pool_queue);
 }
 
+/**
+ * The "main" for each thread. Here, threads start executing, poll the task
+ * queue for work to do continuously, perform tasks as they dequeue them, and
+ * clean up their state if the parent indicates that there exists no more work 
+ * to do.
+ */
 void* thread_launcher(void* args) {
     task_queue* queue = (task_queue*) args;
     errno = 0; // Since errno not guaranteed to be zero-initialized
@@ -406,5 +471,7 @@ void* thread_launcher(void* args) {
         free(next_task);
     }
 
+    // Return value not important here; all desired output effectively produced
+    // as "side effects" of this routine.
     return NULL;
 }
--- src/mustang/mustang_threading.h
+++ src/mustang/mustang_threading.h
@@ -69,10 +69,58 @@ GNU licenses can be found at http://www.gnu.org/licenses/.
 
 extern size_t id_cache_capacity;
 
+/**
+ * Open the file at `path` using the context of the current MarFS position
+ * `current_position` and the associated MDAL `current_mdal`. Then, query the 
+ * string representation of the file's FTAG by getting the xattr with key 
+ * "FTAG_NAME" and return the result (if valid and matching the expected 
+ * length from an initial check) as a heap-allocated string. Close the file 
+ * before returning.
+ *
+ * Returns: pointer to a valid FTAG string representation on success, or NULL 
+ * on failure.
+ *
+ * NOTE: This function may return NULL and potentially set errno in any of the
+ * following cases:
+ * - The file open call using current_mdal->open() fails.
+ * - The first extended attribute query using current_mdal->fgetxattr() fails
+ *   (errno set to ENOATTR, which is reported as ENODATA if the system does not
+ *   define ENOATTR).
+ * - The calloc() call to get heap space for the FTAG string representation 
+ *   fails (errno set to ENOMEM).
+ * - The length of the string value returned in the second 
+ *   current_mdal->fgetxattr() call does not match the expected length as 
+ *   returned from the first current_mdal->fgetxattr() call (errno set to 
+ *   ESTALE).
+ */
 char* get_ftag(marfs_position* current_position, MDAL current_mdal, char* path);
 
+/**
+ * Using a task's parameters, traverse the directory that the current setting 
+ * of `task_position` corresponds to, reading all directory entries and acting
+ * accordingly. If an entry corresponds to a regular file, get its FTAG and its
+ * object ID(s), then store the ID(s). If an entry corresponds to a directory, 
+ * create a new task for that directory bundled with appropriate state for a 
+ * worker thread to complete.
+ *
+ * NOTE: This function always returns, including on failure. Failures are 
+ * always logged to the logfile passed as a program argument since all 
+ * build settings at least log errors.
+ */
 void traverse_dir(marfs_config* base_config, marfs_position* task_position, hashtable* output_table, pthread_mutex_t* table_lock, task_queue* pool_queue);
 
+/**
+ * Using a task's parameters, check the namespace that the current setting of
+ * `task_position` corresponds to for subspaces. Create new tasks, bundled with
+ * appropriate state, for any subspaces (i.e., nested namespaces) for worker 
+ * threads to complete. Then, treat this namespace as a directory and check for
+ * "standard" (i.e., regular file and directory) contents by calling 
+ * traverse_dir().
+ *
+ * NOTE: Like traverse_dir(), this function always returns, including on 
+ * failure. Failures are always logged to the relevant logfile since all build
+ * settings for MUSTANG log errors.
+ */
 void traverse_ns(marfs_config* base_config, marfs_position* task_position, hashtable* output_table, pthread_mutex_t* table_lock, task_queue* pool_queue);
 
 #endif
